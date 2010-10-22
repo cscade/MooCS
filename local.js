81327c1bc3dbdec54d262c47f78ee73cce3d59ce
Carson Christian <cscade@gmail.com>
2010-10-22 18:06:51 -0400

    Test page cosmetics

--- local.js
+++ local.js
@@ -15,12 +15,15 @@ window.addEvent('domready', function () {
 	testRead = function (hard) {
 		var output = document.id('output').empty();
 		Object.each(BCS.Comm.list('read'), function (keys, section) {
-			output.grab(new Element('h1', {
+			var sectionEl = new Element('div.section');
+			
+			output.grab(sectionEl);
+			sectionEl.grab(new Element('h1', {
 				text: section
 			}));
 			keys.each(function (key) {
 				BCS.Comm[(hard) ? 'hardRead' : 'read'](section, key, function (response) {
-					output.grab(new Element('p', {
+					sectionEl.grab(new Element('p', {
 						html: key + ': <strong>' + response + '</strong>'
 					}));
 				});
