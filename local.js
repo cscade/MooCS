fc51487dfce1ad5abe986313cb78fd8addc8518c
Carson Christian <cscade@gmail.com>
2010-10-22 17:11:52 -0400

    Additional test page setup

--- local.js
+++ local.js
@@ -1,14 +1,26 @@
 window.addEvent('domready', function () {
-	Object.each(BCS.Comm.list('read'), function (keys, section) {
-		document.body.grab(new Element('h1', {
-			text: section
-		}));
-		keys.each(function (key) {
-			BCS.Comm.read(section, key, function (response) {
-				document.body.grab(new Element('p', {
-					html: key + ': <strong>' + response + '</strong>'
-				}));
+	var testRead;
+	
+	document.id('refresh').addEvent('click', function () {
+		testRead();
+	});
+	
+	$$('div.stackLeft.library span').set('text', BCS.$libraryVersion);
+	$$('div.stackLeft.target span').set('text', BCS.$location);
+	
+	testRead = function () {
+		var output = document.id('output').empty();
+		Object.each(BCS.Comm.list('read'), function (keys, section) {
+			output.grab(new Element('h1', {
+				text: section
+			}));
+			keys.each(function (key) {
+				BCS.Comm.read(section, key, function (response) {
+					output.grab(new Element('p', {
+						html: key + ': <strong>' + response + '</strong>'
+					}));
+				});
 			});
 		});
-	});
+	};
 });
