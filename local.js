885e5e5d3a021c628cdad6195b511e005699eaad
Carson Christian <cscade@gmail.com>
2010-10-22 16:57:37 -0400

    Rename BCS.Core -> BCS.Comm

--- local.js
+++ local.js
@@ -1,10 +1,10 @@
 window.addEvent('domready', function () {
-	Object.each(BCS.Core.list('read'), function (keys, section) {
+	Object.each(BCS.Comm.list('read'), function (keys, section) {
 		document.body.grab(new Element('h1', {
 			text: section
 		}));
 		keys.each(function (key) {
-			BCS.Core.read(section, key, function (response) {
+			BCS.Comm.read(section, key, function (response) {
 				document.body.grab(new Element('p', {
 					text: key + ': ' + response
 				}));
