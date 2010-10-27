8bfeba0d18e8579ced2b1a851b2baa69442912f0
Carson Christian <cscade@gmail.com>
2010-10-27 16:40:52 -0400

    Add charting

--- local.js
+++ local.js
@@ -11,13 +11,28 @@
 	or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
 window.addEvent('domready', function () {
-	var testRead;
+	var testRead,
+		doChart, chart,
+		chartData0 = [],
+		chartData1 = [],
+		chartData2 = [],
+		chartData3 = [],
+		chartCounter = 0,
+		autoUpdate;
 	
 	$$('div.stackRight.library span').set('text', BCS.$libraryVersion);
 	$$('div.stackRight.target span').set('text', BCS.$location);
 	
+	BCS.Comm.read('firmware', 'version', function (response) {
+		$$('div.stackRight.firmware span')[0].set('text', response);
+	});
+	
 	document.id('showAll').addEvent('click', function () {
 		$$('div.section input').fireEvent('click');
+	});
+	
+	document.id('drawChart').addEvent('click', function () {
+		autoUpdate();
 	});
 	
 	testRead = function (hard) {
@@ -44,4 +59,49 @@ window.addEvent('domready', function () {
 	
 	// Initial load
 	testRead();
+	
+	// Chart
+	chart = new JSChart('chart', 'line');
+	chart.setTitle('');
+	chart.setAxisNameX('Seconds Elapsed');
+	chart.setAxisNameY('F');
+	doChart = function () {
+		chartCounter += 1;
+		BCS.Comm.read('temp', 'probe0', function (r) {
+			chartData0.push([chartCounter, Math.round(r)]);
+			if (chartData0.length > 2000) {
+				chartData0.splice(0, chartData0.length - 2000);
+			}
+		});
+		BCS.Comm.read('temp', 'probe1', function (r) {
+			chartData1.push([chartCounter, Math.round(r)]);
+			if (chartData1.length > 2000) {
+				chartData1.splice(0, chartData1.length - 2000);
+			}
+		});
+		BCS.Comm.read('temp', 'probe2', function (r) {
+			chartData2.push([chartCounter, Math.round(r)]);
+			if (chartData2.length > 2000) {
+				chartData2.splice(0, chartData2.length - 2000);
+			}
+		});
+		BCS.Comm.read('temp', 'probe3', function (r) {
+			chartData3.push([chartCounter, Math.round(r)]);
+			if (chartData3.length > 2000) {
+				chartData3.splice(0, chartData3.length - 2000);
+			}
+		});
+	}.periodical(1000);
+	autoUpdate = function () {
+		chart.setDataArray(chartData0, '0');
+		chart.setDataArray(chartData1, '1');
+		chart.setDataArray(chartData2, '2');
+		chart.setDataArray(chartData3, '3');
+		chart.setLineColor('#961703', '0');
+		chart.setLineColor('#331595', '1');
+		chart.setLineColor('#3c7d96', '2');
+		chart.setLineColor('#4b9531', '3');
+		chart.draw();
+	}.periodical(10000);
+	
 });
