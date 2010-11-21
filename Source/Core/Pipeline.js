86a0556cd8bea0656582db99ee233e743202eac8
Carson Christian <cscade@gmail.com>
2010-11-21 10:42:30 -0500

    Pipeline Class operational [#2 state:resolved]

--- Source/Core/Pipeline.js
+++ Source/Core/Pipeline.js
@@ -20,7 +20,8 @@ authors: [Carson S. Christian](mailto:cchristian@moocsinterface.net)
 MooCS.Pipeline = new Class({
 	Implements: [Options],
 	options: {
-		cacheAge : 500
+		cacheAge : 500,
+		timeout: 5000
 	},
 	
 	initialize: function (location, translator, options) {
@@ -29,7 +30,6 @@ MooCS.Pipeline = new Class({
 		this.translatorUrl = translator;
 		this.requests = {};
 		this.cache = {};
-		this.commLog = { responseTimes: [], duplicatePolls: 0, timeouts: 0, exceptions: 0 };
 	},
 	
 	read: function (decoder) {
@@ -39,10 +39,10 @@ MooCS.Pipeline = new Class({
 			decoder.go(this.getCache(decoder.options.target));
 			return;
 		}
-		if (!this.checkRequests(decoder.options.target)) {
-			this.generateRequest(decoder);
+		if (this.checkRequests(decoder.options.target)) {
+			this.watchRequest(decoder);
 		} else {
-			// Watch existing
+			this.generateRequest(decoder);
 		}
 	},
 	
@@ -72,9 +72,11 @@ MooCS.Pipeline = new Class({
 		if (this.requests[structure] === undefined) {
 			return false;
 		}
+		return true;
 	},
 	
 	generateRequest: function (decoder) {
+		// Create a new request and and it to the queue
 		var request = new Request({
 			url: this.translatorUrl,
 			data: Object.toQueryString({ location: this.deviceUrl, target: decoder.options.target, mode: 'get', message: null }),
@@ -82,9 +84,73 @@ MooCS.Pipeline = new Class({
 		});
 		request.addEvent('success', function (r) {
 			decoder.go(r);
-			this.updateCache(decoder.options.target, responseText);
+			this.updateCache(decoder.options.target, r);
+			this.deleteCurrentRequest();
 		}.bind(this));
+		request.timeStamp = Date.now();
 		this.requests[decoder.options.target] = request;
+		this.start();
+	},
+	
+	watchRequest: function (decoder) {
+		// Watch for the return value of an existing request
+		this.requests[decoder.options.target].addEvent('success', function (r) {
+			decoder.go(r);
+		});
+	},
+	
+	deleteCurrentRequest: function () {
+		// Clear the current request from the pipeline
+		delete this.current;
+	},
+	
+	getNextRequest: function () {
+		// Return the oldest request in the queue
+		var age = null, oldest = undefined, key = null;
+		
+		Object.each(this.requests, function (r, k) {
+			if (age === null || r.timeStamp < age) {
+				age = r.timeStamp;
+				oldest = r;
+				key = k;
+			}
+		});
+		delete this.requests[key];
+		return oldest;
+	},
+	
+	runRequest: function () {
+		if (this.current !== undefined) {
+			var age = Date.now() - this.current.startTime;
+			if (age > this.options.timeout) {
+				this.deleteCurrentRequest();
+			}
+		} else {
+			if (Object.getLength(this.requests) > 0) {
+				this.current = { startTime: Date.now(), request: this.getNextRequest() };
+				this.current.request.send();
+			} else {
+				this.stop();
+			}
+		}
+	},
+	
+	start: function () {
+		// Start the pipeline
+		var host = this;
+		
+		if (this.runner !== undefined) {
+			return;
+		}
+		this.runner = (function () {
+			host.runRequest();
+		}).periodical(1);
+	},
+	
+	stop: function () {
+		// Stop the pipeline
+		clearTimeout(this.runner);
+		delete this.runner;
 	}
 	
 });
