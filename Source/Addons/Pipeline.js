9c6708c489c7d72e287028766d1381e9307d7369
Carson Christian <cscade@gmail.com>
2010-11-21 17:16:25 -0500

    Pipeline.Stats now recording data [#6]

--- Source/Addons/Pipeline.js
+++ Source/Addons/Pipeline.js
@@ -8,6 +8,7 @@ description: Provides additional, optional functionality to the Pipeline class.
 license: Creative Commons Attribution-ShareAlike 3.0 Unported License.
 
 requires:
+	- More/Class.refactor
 	- /Pipeline
 
 provides: [Pipeline.Stats]
@@ -16,17 +17,51 @@ authors: [Carson S. Christian](mailto:cchristian@moocsinterface.net)
 
 ...
 */
-Pipeline.implement({
+MooCS.Pipeline = Class.refactor(MooCS.Pipeline, {
 	// Pipeline.Stats
-	recordResponseTime: function (timestamp) {
-		// Record the response time in the communications log
-		if (timestamp === undefined || Date.now() === timestamp) {
-			return;
+	
+	initialize: function (location, translator, options) {
+		this.stats = {
+			read: {
+				requests: 0,
+				timeouts: 0,
+				lastRequestTime: 0,
+				responseTimes: []
+			},
+			write: {
+				requests: 0,
+				timeouts: 0,
+				lastRequestTime: 0,
+				responseTimes: []
+			}
+		};
+		this.previous(location, translator, options);
+	},
+	
+	attachEvents: function (request, decoder) {
+		request.addEvent('success', function (r) {
+			this.recordResponseTime();
+		}.bind(this));
+		this.previous(request, decoder);
+	},
+	
+	send: function () {
+		this.stats.read.requests += 1;
+		this.stats.read.lastRequestTime = Date.now();
+		this.previous();
+	},
+	
+	timeout: function () {
+		this.stats.read.timeouts += 1;
+		this.previous();
+	},
+	
+	recordResponseTime: function () {
+		// Record the response time in the stats
+		if (this.stats.read.responseTimes.length > 59) {
+			this.stats.read.responseTimes.splice(0, 1);
 		}
-		if (this.communications.responseTimes.length > 59) {
-			this.communications.responseTimes.splice(0, 1);
-		}
-		this.communications.responseTimes.push((Date.now() - timestamp) / 1000);
+		this.stats.read.responseTimes.push((Date.now() - this.stats.read.lastRequestTime) / 1000);
 	}
 	
 });
