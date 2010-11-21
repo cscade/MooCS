5f28a0fea889eed87839e0ce1bdf698c4d9f9935
Carson Christian <cscade@gmail.com>
2010-11-21 11:46:13 -0500

    New class Poll

---
+++ Source/Core/Poll.js
@@ -0,0 +1,26 @@
+/*
+---
+
+name: Poll
+
+description: A polling object that can repetitively refresh structures.
+
+license: Creative Commons Attribution-ShareAlike 3.0 Unported License.
+
+requires:
+	- /MooCS
+	- /Pipeline
+
+provides: [Poll]
+
+authors: [Carson S. Christian](mailto:cchristian@moocsinterface.net)
+
+...
+*/
+MooCS.Poll = new Class({
+	
+	initialize: function () {
+		
+	}
+	
+});
