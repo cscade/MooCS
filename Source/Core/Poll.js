365d9f1a8518c29427507dc16bc2afd7ece3b650
Carson Christian <cscade@gmail.com>
2010-11-23 17:15:42 -0500

    [#5 state:resolved] Poll class now functional

--- Source/Core/Poll.js
+++ Source/Core/Poll.js
@@ -10,6 +10,7 @@ license: Creative Commons Attribution-ShareAlike 3.0 Unported License.
 requires:
 	- /MooCS
 	- /Pipeline
+	- Core/Class.Extras
 
 provides: [Poll]
 
@@ -18,9 +19,45 @@ authors: [Carson S. Christian](mailto:cchristian@moocsinterface.net)
 ...
 */
 MooCS.Poll = new Class({
+	Implements: [Options, Events],
+	options: {
+		interval: 1000	// Delay in ms between polls
+	},
 	
-	initialize: function () {
+	initialize: function (device, options) {
+		this.device = device;
+		this.setOptions(options);
+	},
+	
+	run: function () {
+		var host = this;
+		// The main run loop. Executes every this.options.interval after calling start()
+		if (Object.getLength(this.device.listeners) < 1) return;
+		Object.each(this.device.listeners, function (listeners) {
+			listeners.each(function (decoder) {
+				host.device.pipeline.read(decoder);
+			});
+		});
+	},
+	
+	start: function () {
+		// Start the poller
+		var host = this;
 		
+		if (this.runner !== undefined) {
+			return;
+		}
+		this.runner = (function () {
+			host.run();
+		}).periodical(this.options.interval);
+		this.fireEvent('start');
+	},
+	
+	stop: function () {
+		// Stop the poller
+		clearTimeout(this.runner);
+		delete this.runner;
+		this.fireEvent('stop');
 	}
 	
 });
